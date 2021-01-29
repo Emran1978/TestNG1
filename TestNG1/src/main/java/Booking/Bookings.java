59df0f376dd55008ffb5c7f3bf9be7510c23612c
emran <emran@LAPTOP-B65CD29P.fios-router.home>
2021-01-29 02:11:26 -0500

    new code added

---
+++ TestNG1/src/main/java/Booking/Bookings.java
@@ -0,0 +1,12 @@
+package Booking;
+
+import org.testng.annotations.Test;
+
+public class Bookings {
+
+	@Test
+	public void booking() {
+		
+		System.out.println("booking");
+	}
+}
---
+++ TestNG1/src/test/resources/BookAFlight.java
@@ -0,0 +1,11 @@
+
+public class BookAFlight {
+	
+	
+	@Test
+	public void bookigTC1() {
+		
+		System.out.println("booking");
+	}
+
+}
